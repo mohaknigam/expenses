601e1bc56308a1d13aa13e93f517fde6db85d875
mohak <mohaknigam20@gmail.com>
2021-02-26 14:12:11 +0530

    Revert "ouy"
    This reverts commit 2d55740b

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'widgets/new_transaction.dart';
 import 'models/transactions.dart';
 import 'widgets/transaction_list.dart';
 import 'widgets/chart.dart';
-import 'package:fintrack/widgets/PageBody.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,97 +39,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // final List<Transaction> transactions = [
-  //   Transaction(
-  //       title: 'New Shows',
-  //       id: DateTime.now().toString(),
-  //       amount: 23,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New Shoes',
-  //       id: DateTime.now().toString(),
-  //       amount: 24,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New Shirt',
-  //       id: DateTime.now().toString(),
-  //       amount: 30,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New cow',
-  //       id: DateTime.now().toString(),
-  //       amount: 34,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New Shall',
-  //       id: DateTime.now().toString(),
-  //       amount: 21,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New town',
-  //       id: DateTime.now().toString(),
-  //       amount: 42,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New Shows2',
-  //       id: DateTime.now().toString(),
-  //       amount: 25,
-  //       date: DateTime.now()),
-  //   Transaction(
-  //       title: 'New Shows3',
-  //       id: DateTime.now().toString(),
-  //       amount: 213,
-  //       date: DateTime.now()),
-  // ];
+  final List<Transaction> _transactions = [
+    Transaction(
+        title: 'New Shows',
+        id: DateTime.now().toString(),
+        amount: 23,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New Shoes',
+        id: DateTime.now().toString(),
+        amount: 24,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New Shirt',
+        id: DateTime.now().toString(),
+        amount: 30,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New cow',
+        id: DateTime.now().toString(),
+        amount: 34,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New Shall',
+        id: DateTime.now().toString(),
+        amount: 21,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New town',
+        id: DateTime.now().toString(),
+        amount: 42,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New Shows2',
+        id: DateTime.now().toString(),
+        amount: 25,
+        date: DateTime.now()),
+    Transaction(
+        title: 'New Shows3',
+        id: DateTime.now().toString(),
+        amount: 213,
+        date: DateTime.now()),
+  ];
 
-  // bool _showChart = false;
+  bool _showChart = false;
 
-  // List<Transaction> get _recentTransactions {
-  //   return transactions.where((tx) {
-  //     return tx.date.isAfter(
-  //       DateTime.now().subtract(
-  //         Duration(days: 7),
-  //       ),
-  //     );
-  //   }).toList();
-  // }
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
-  // void _addNewTransaction(
-  //     String txTitle, double txAmount, DateTime chosenDate) {
-  //   final newTx = Transaction(
-  //     title: txTitle,
-  //     id: DateTime.now().toString(),
-  //     amount: txAmount,
-  //     date: chosenDate,
-  //   );
-  //
-  //   setState(
-  //     () {
-  //       transactions.insert(0, newTx); // to insert new element at top always
-  //     },
-  //   );
-  // }
+  void _addNewTransaction(
+      String txTitle, double txAmount, DateTime chosenDate) {
+    final newTx = Transaction(
+      title: txTitle,
+      id: DateTime.now().toString(),
+      amount: txAmount,
+      date: chosenDate,
+    );
 
-  // void _startAddingNewTransaction(BuildContext ctx) {
-  //   showModalBottomSheet(
-  //     context: ctx,
-  //     builder: (bCtx) {
-  //       return GestureDetector(
-  //         onTap: () {},
-  //         child: NewTransaction(
-  //           addTx: _addNewTransaction,
-  //         ),
-  //         behavior: HitTestBehavior.opaque,
-  //       );
-  //     },
-  //   );
-  // }
-  //
-  // void _deleteTransaction(String id) {
-  //   setState(() {
-  //     transactions.removeWhere((tx) => tx.id == id);
-  //   });
-  // }
+    setState(
+      () {
+        _transactions.insert(0, newTx); // to insert new element at top always
+      },
+    );
+  }
+
+  void _startAddingNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (bCtx) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(
+            addTx: _addNewTransaction,
+          ),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tx) => tx.id == id);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -138,41 +137,89 @@ class _HomePageState extends State<HomePage> {
 
     final isLandscape = media.orientation == Orientation.landscape;
 
-    // final appBar = AppBar(
-    //   actions: [
-    //     IconButton(
-    //       splashColor: Colors.yellow,
-    //       iconSize: 30,
-    //       icon: Icon(Icons.add),
-    //       onPressed: () => _startAddingNewTransaction(context),
-    //       color: Colors.yellow,
-    //     ),
-    //   ],
-    //   title: Text(
-    //     'FinTrack',
-    //   ),
-    //   centerTitle: true,
-    // );
+    final appBar = AppBar(
+      actions: [
+        IconButton(
+          splashColor: Colors.yellow,
+          iconSize: 30,
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddingNewTransaction(context),
+          color: Colors.yellow,
+        ),
+      ],
+      title: Text(
+        'FinTrack',
+      ),
+      centerTitle: true,
+    );
 
-    // final txListWidget = Container(
-    //   height: (media.size.height -
-    //           appBar.preferredSize.height -
-    //           media.padding.top) *
-    //       0.65, //65%
-    //   child: TransactionList(
-    //     transactions: transactions,
-    //     deleteTx: _deleteTransaction,
-    //   ),
-    // );
+    final txListWidget = Container(
+      height: (media.size.height -
+              appBar.preferredSize.height -
+              media.padding.top) *
+          0.65, //65%
+      child: TransactionList(
+        transactions: _transactions,
+        deleteTx: _deleteTransaction,
+      ),
+    );
 
-    final pageBody = PageBody();
+    final pageBody = SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Show Chart',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Switch.adaptive(
+                    value: _showChart,
+                    onChanged: (val) {
+                      setState(() {
+                        _showChart = val;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (!isLandscape)
+              Container(
+                height: (media.size.height -
+                        appBar.preferredSize.height -
+                        media.padding.top) *
+                    0.35, //35%
+                child: Chart(
+                  recentTransactions: _recentTransactions,
+                ),
+              ),
+            if (!isLandscape) txListWidget,
+            if (isLandscape)
+              _showChart
+                  ? Container(
+                      height: (media.size.height -
+                              appBar.preferredSize.height -
+                              media.padding.top) *
+                          0.7, //35%
+                      child: Chart(
+                        recentTransactions: _recentTransactions,
+                      ),
+                    )
+                  : txListWidget,
+          ],
+        ),
+      ),
+    );
 
     return Scaffold(
       appBar: appBar,
       body: pageBody,
       floatingActionButton: FloatingActionButton(
         splashColor: Colors.yellow,
-        onPressed: () => startAddingNewTransaction(context),
+        onPressed: () => _startAddingNewTransaction(context),
         child: Icon(
           Icons.add,
           color: Colors.yellow,
